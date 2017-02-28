38bfff21f432f5b689186f99980f5f6402d9c66f
Piotr Frankiewicz <frankiewicz@o2.pl>
2017-02-28 22:16:57 +0100

    Adding Mind Map

---
+++ Documents/Mind Map/Komunikator.mm
@@ -0,0 +1,123 @@
+
+<map version="0.9.0">
+    <node TEXT="Komunikator" FOLDED="false">
+        <edge COLOR="#b4b4b4" />
+        <font NAME="Helvetica" SIZE="10" />
+        <node TEXT="Aplikacja" FOLDED="false" POSITION="left">
+            <edge COLOR="#67d7c4" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Na telefon" FOLDED="false">
+                <edge COLOR="#6bd5cb" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Przenośna" FOLDED="false">
+                    <edge COLOR="#66cbd2" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="Na komputery osobiste" FOLDED="false">
+                <edge COLOR="#60d4b8" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Do użycia w domu" FOLDED="false">
+                    <edge COLOR="#57d3ca" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="Wygodna w użyciu" FOLDED="false">
+                <edge COLOR="#5ed4d6" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Przejrzysta" FOLDED="false">
+                    <edge COLOR="#63d9d8" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Estetyczna" FOLDED="false">
+                    <edge COLOR="#5abfd2" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+        </node>
+        <node TEXT="Rozmowa" FOLDED="false" POSITION="right">
+            <edge COLOR="#7aa3e5" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Z kimś obcym" FOLDED="false">
+                <edge COLOR="#7595e2" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Dla zabawy" FOLDED="false">
+                    <edge COLOR="#7c8be3" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Coś nowego" FOLDED="false">
+                    <edge COLOR="#7fb2e4" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="Ze znajomymi" FOLDED="false">
+                <edge COLOR="#789ae5" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Kameralnie" FOLDED="false">
+                    <edge COLOR="#7d93e4" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Swobodnie" FOLDED="false">
+                    <edge COLOR="#7896e7" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="Burza mózgów" FOLDED="false">
+                <edge COLOR="#7a9be5" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Dyskusja" FOLDED="false">
+                <edge COLOR="#7892e5" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Wymiana zdań" FOLDED="false">
+                <edge COLOR="#84abe5" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="Internetowy" FOLDED="false" POSITION="left">
+            <edge COLOR="#ebd95f" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Łatwo dostepny" FOLDED="false">
+                <edge COLOR="#ebd86b" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="Powiadomienie" FOLDED="false" POSITION="left">
+            <edge COLOR="#9ed56b" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Wiadomość" FOLDED="false">
+                <edge COLOR="#a4d56d" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Zdjęcie" FOLDED="false">
+                    <edge COLOR="#97d46c" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Informacja" FOLDED="false">
+                    <edge COLOR="#b4d773" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Tekst" FOLDED="false">
+                    <edge COLOR="#b7d672" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="Status" FOLDED="false">
+                <edge COLOR="#96d86c" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Dźwiękowe" FOLDED="false">
+                <edge COLOR="#88d369" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Dzwonek" FOLDED="false">
+                    <edge COLOR="#93d36b" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Melodyjka" FOLDED="false">
+                    <edge COLOR="#7fd573" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+        </node>
+    </node>
+</map>
