8cdcb99aeee3826638f71765880e83df4213332c
rozPierog <rozpierog@gmail.com>
2017-03-28 15:23:12 +0200

    Added Notifications if the app is in the background

---
+++ Mobile/Communicator/app/src/main/java/com/example/rozpi/communicator/EmptyActivity.java
@@ -0,0 +1,13 @@
+package com.example.rozpi.communicator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finish();
+    }
+}
